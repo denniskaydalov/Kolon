de7ea6adae9fafd6c5e5520968f3217794424839
Dennis Kaydalov <denniskaydalov@gmail.com>
2021-08-22 16:08:54 -0400

    Initial commit

---
+++ KolonApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KolonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
